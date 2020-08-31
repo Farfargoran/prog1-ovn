f30804786572f43a6e80fcf21febc5e8510ca961
Farfargoran <70516839+Farfargoran@users.noreply.github.com>
2020-08-31 14:42:52 +0200

    kap2test

--- Uppgift1.4.py
+++ Uppgift1.4.py
@@ -1,7 +1,7 @@
 # uppgift1.4.py
 
-print(Hej)
-print(Välkommen till python)
+print("Hej")
+print("Välkommen till python" )
 name = input ("vad heter du? ")
 print("Hej", name)
 name = input ("vad heter du i efternamn? ")
