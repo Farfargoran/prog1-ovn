476e9a6abc9a645ad9b80e8dd1849c6a6b690d06
Farfargoran <70516839+Farfargoran@users.noreply.github.com>
2020-08-31 14:15:39 +0200

    övning 1.4

---
+++ Uppgift1.4.py
@@ -0,0 +1,8 @@
+# uppgift1.4.py
+
+print(Hej)
+print(Välkommen till python)
+name = input ("vad heter du? ")
+print("Hej", name)
+name = input ("vad heter du i efternamn? ")
+name = input ("vad är din favorit sport? ")
